54738dc25039df0172bd1b82f9437ad4b349d6aa
BASEEM ALTWAJRE <32454458+altwajre@users.noreply.github.com>
2019-01-05 11:40:36 +0100

    Update AkkaMCNonBlockingBoundedActorSpec.scala

--- scr/test/scala/com/github/altwajre/actors/AkkaMCNonBlockingBoundedActorSpec.scala
+++ scr/test/scala/com/github/altwajre/actors/AkkaMCNonBlockingBoundedActorSpec.scala
@@ -1,1 +1,29 @@
+package com.github.altwajre.actors
 
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory._
+
+class AkkaMCNonBlockingBoundedActorSpec extends AkkaBoundedActorSpec {
+  override def config: Config = load(parseString(
+    """
+      akka {
+        log-dead-letters = 0
+        log-dead-letters-during-shutdown = off
+        actor {
+          unstarted-push-timeout = 100s
+          benchmark-dispatcher {
+            executor = "com.github.altwajre.actors.CustomExecutorServiceConfigurator"
+            throughput = 1024
+            mailbox-type = "akka.dispatch.MultiConsumerNonBlockingBoundedMailbox"
+            mailbox-capacity = 10000000
+          }
+          benchmark-dispatcher-2 {
+            executor = "com.github.altwajre.actors.CustomExecutorServiceConfigurator"
+            throughput = 1024
+            mailbox-type = "akka.dispatch.MultiConsumerNonBlockingBoundedMailbox"
+            mailbox-capacity = 1
+          }
+        }
+      }
+    """))
+}
