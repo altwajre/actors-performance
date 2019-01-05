f236d7faabd9e4f119b18038dd0e016a56ca514b
BASEEM ALTWAJRE <32454458+altwajre@users.noreply.github.com>
2019-01-05 12:38:09 +0100

    Update ConcurrencyUtils.scala

--- scr/test/scala/com/github/altwajre/actors/ConcurrencyUtils.scala
+++ scr/test/scala/com/github/altwajre/actors/ConcurrencyUtils.scala
@@ -1,1 +1,34 @@
+package com.github.altwajre.actors
 
+import java.util.concurrent._
+import akka.dispatch.ForkJoinExecutorConfigurator.AkkaForkJoinPool
+
+class ParRunner(fs: Seq[() => Unit]) {
+  val barrier = new CyclicBarrier(fs.size + 1)
+  fs.map(f => new Thread {
+    setDaemon(true)
+
+    override def run(): Unit = {
+      barrier.await()
+      f()
+    }
+  }).foreach(_.start())
+
+  def start(): Unit = barrier.await()
+}
+
+abstract class JavaForkJoinTask(p: ForkJoinPool) extends ForkJoinTask[Unit] {
+  p.execute(this)
+
+  def getRawResult: Unit = ()
+
+  def setRawResult(unit: Unit): Unit = ()
+}
+
+abstract class AkkaForkJoinTask(p: AkkaForkJoinPool) extends akka.dispatch.forkjoin.ForkJoinTask[Unit] {
+  p.execute(this)
+
+  def getRawResult: Unit = ()
+
+  def setRawResult(unit: Unit): Unit = ()
+}
