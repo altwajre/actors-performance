e85378d5db87d6315020d88251056e041dc8811d
BASEEM ALTWAJRE <32454458+altwajre@users.noreply.github.com>
2019-01-05 11:43:54 +0100

    Update AkkaMCUnboundedActorSpec.scala

--- scr/test/scala/com/github/altwajre/actors/AkkaMCUnboundedActorSpec.scala
+++ scr/test/scala/com/github/altwajre/actors/AkkaMCUnboundedActorSpec.scala
@@ -1,1 +1,22 @@
+package com.github.altwajre.actors
 
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory._
+
+class AkkaMCUnboundedActorSpec extends AkkaUnboundedActorSpec {
+  override def config: Config = load(parseString(
+    """
+      akka {
+        log-dead-letters = 0
+        log-dead-letters-during-shutdown = off
+        actor {
+          unstarted-push-timeout = 100s
+          benchmark-dispatcher {
+            executor = "com.github.altwajre.actors.CustomExecutorServiceConfigurator"
+            throughput = 1024
+            mailbox-type = "akka.dispatch.MultiConsumerUnboundedMailbox"
+          }
+        }
+      }
+    """))
+}
